13875f34b7aeb7d33a2c3b89a6054936105d7bba
rae.mcphail <rae.mcphail@ucalgary.ca>
2022-10-19 12:38:56 -0600

    Removed print statements

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function toggleDrawer(){
     let drawer = document.getElementById("drawer");
-    console.log("Display is: ")
-    console.log(drawer.style.display);
     if (drawer.style.display === "block") {
         drawer.style.display = "none";
     } else {
